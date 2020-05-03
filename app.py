c20f599e913bb7a460fe0f74c5a168f4b11628a3
Abhishek Das <factorwonk@protonmail.com>
2020-05-04 01:21:55 +1000

    Updated Dash code to display sentiment chart for financial services companies related to Covid-19 related news over time.

--- app.py
+++ app.py
@@ -1,34 +1,33 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.express as px
 import pandas as pd
-import numpy as np
+
 
 # PATH to csv file
 PATH = 'client_sentiment.csv'
 df = pd.read_csv(PATH).drop('Unnamed: 0', axis=1).reset_index()
-
-def generate_table(dataframe, max_rows=10):
-    return html.Table([
-        html.Thead(
-            html.Tr([html.Th(col) for col in dataframe.columns])
-        ),
-        html.Tbody([
-            html.Tr([
-                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
-            ]) for i in range(min(len(dataframe), max_rows))
-        ])
-    ])
+# Transform the dataframe to get the mean daily VADER Score for each client
+df = df.groupby(['Date','Client'])['VADER Score'].mean().reset_index()
+df['Date'] = pd.to_datetime(df['Date'])
+df = df[df.Date > '2020-03-01']
+fig = px.line(df, x='Date', y='VADER Score', color='Client')
+fig.update_traces(mode='markers+lines')
+#fig.show()
 
 # Import CSS sheets from here
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = html.Div(children=[
-    html.H4(children='Covid-19 related news for customers'),
-    generate_table(df)
+app.layout = html.Div([
+    html.H3("Market sentiment related to companies over time based on Covid-19 related news"),
+    dcc.Graph(
+        id = 'SentimentChart',
+        figure=fig
+    )
 ])
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
